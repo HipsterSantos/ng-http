993e973c525413ec8fd0d55e9051c0cb487b1acb
HipsterSantos <santoscampos269@gmail.com>
2024-04-02 18:08:11 +0100

    set get , delete,post : set http methods and simulating requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './post.model';
+import { Subject, Subscription, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ng-http';
+export class AppComponent implements OnInit {
+  url = `https://api.thecatapi.com/v1/breeds`;
+  api_key = "live_ccoozsev8nF86SL1kr8lU7nO7CJ6Whb2otwi0qmMwpAm3HRAHUYqAT1pxAUPtilM"
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  sub = new Subject();
+  subscribeable: Subscription;
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+  onCreatePost(postData?:Post){
+    this.http.post('url',{
+      headers: {
+      'x-api-key': this.api_key
+     }
+  }).subscribe(data=>{
+      console.log('data was posted')
+    },
+    error=>{
+      this.error = error.message
+      this.sub.next(error)
+      console.log('subs--',this.sub)
+    })
+  }
+  onFetchPosts(){
+    this.fetchPosts();
+  }
+  onClearPosts(){
+
+  }
+
+  private fetchPosts(){
+    this.isFetching = true;
+    this.http.get('url').pipe( map(toPipe=>{
+      return toPipe
+    })).subscribe( pipedData=>{
+      this.isFetching = false;
+      // this.loadedPosts = (pipedData) as Post
+    })
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModuleModule } from './app/app.module.module';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+platformBrowserDynamic().bootstrapModule(AppModuleModule)
+.catch(console.error)
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
